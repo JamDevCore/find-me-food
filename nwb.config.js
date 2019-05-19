694e9649bdee1f6e548e5381c6726f704f195397
James Harding <jamesvitaly1993@gmail.com>
2019-05-19 12:17:55 +0100

    Updated config file

--- nwb.config.js
+++ nwb.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extra: {
       plugins: [
         new webpack.EnvironmentPlugin({
-          GOOGLE_MAPS_API_KEY: ""
+          GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
         })
       ]
     }
