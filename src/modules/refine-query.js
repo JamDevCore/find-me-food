a20c3006cb26f819aa45d6d298b368e665179a73
James Harding <jamesvitaly1993@gmail.com>
2019-05-22 17:45:46 +0100

    Removed <<< head from file

--- src/modules/refine-query.js
+++ src/modules/refine-query.js
@@ -5,30 +5,6 @@ const refineQuery = (items) => {
     return b.rating-a.rating;
   });
 
-<<<<<<< HEAD
-
-  //look for radio button choice here.
-
-  //TODO: need to check how it returns the restaurant type (chines indian etc. for sortedItems[x].type)
-  var y = 0;
-  var radioButton;
-  for (radioButton in document.getElementsByClassName("cuisineCheckbox")) {
-    if (radioButton.checked === true) {
-      var type = radioButton.value
-      for (x = 0; x < sortedItems.length; x++) {
-        if (sortedItems[x].type === radiobutton) {
-          y = x;
-          break;
-        }
-      }
-    }
-  }
-
-
-  const bestItem = sortedItems[y];
-
-=======
->>>>>>> master
 
   // //look for radio button choice here.
   //
