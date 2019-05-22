2923c5837d3c66e945c5d40b2f5ed2cc308ceffe
Grant Lee <grantlee289@gmail.com>
2019-05-22 10:59:23 +0100

    Started the radio button refine.

--- src/modules/refine-query.js
+++ src/modules/refine-query.js
@@ -5,7 +5,27 @@ const refineQuery = (items) => {
     return b.rating-a.rating;
   });
 
-  const bestItem = sortedItems[0];
+
+  //look for radio button choice here.
+
+  //TODO: need to check how it returns the restaurant type (chines indian etc. for sortedItems[x].type)
+  var y = 0;
+  var radioButton;
+  for (radioButton in getElementsByClassName("cuisineCheckbox")) {
+    if (!null === radioButton.value) {
+      var type = radioButton.value
+      for (x = 0; x < sortedItems.length; x++) {
+        if (sortedItems[x].type === radiobutton) {
+          y = x;
+          break;
+        }
+      }
+    }
+  }
+
+
+  const bestItem = sortedItems[y];
+
 
   return bestItem;
 
